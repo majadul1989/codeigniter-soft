83ab0a93612a844a8d4dbc6b2469a6fb3366d774
majadul1989 <majadul1989@gmail.com>
2017-10-20 10:30:26 +0600

    stash for merge

--- application/controllers/dashboard/product.php
+++ application/controllers/dashboard/product.php
@@ -8,7 +8,7 @@ class Product extends CI_Controller {
         $this->zend->load('Zend/Barcode');
         $file = Zend_Barcode::draw('code128', 'image', array('text' => '1222'), array());
         $code = time().'1222';
-        $store_image = imagepng($file,"barcode/{$code}.png");
+        imagepng($file,"barcode/{$code}.png");
         $data['barcode'] = $code.'.png';
         $this->load->view('dashboard/products',$data);
 
